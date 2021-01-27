c9ae9dbf7934f46dfb07618924fe73ca263e4612
Carbon752 <78106998+Carbon752@users.noreply.github.com>
2021-01-27 14:32:07 -0500

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,18 @@
+/****************************************
+*Hello.java
+*****************************************
+*Peter Elverson
+*1/27/2021
+*CMSC 255 003
+****************************************/
+
+public class Hello {
+public static void main(String [] args) {
+System.out.println("Peter Elverson");
+System.out.println("Hello");
+System.out.println("Demonstrate java printing text to console");
+System.out.println("1/27/2021");
+System.out.println("CMSC 255-003");
+System.out.println();
+}
+}
